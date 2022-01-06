2cb8337aeb67035d013145ef35f71902ecaa6f7c
MKTunca <91753517+MKTunca@users.noreply.github.com>
2022-01-06 18:16:18 +0300

    Add files via upload

---
+++ Pets.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace InterfaceJson
+{
+    interface IPetshop
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+    }
+    class Person : IPetshop
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string StateOfOrigin { get; set; }
+
+        public List<Pets> Pet { get; set; }
+    }
+    class Pets : IPetshop
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Type { get; set; }
+    }
+    class PersonMenager
+    {
+        public void Add(Person person)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            Console.WriteLine(JsonSerializer.Serialize(person, options));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace InterfaceJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonMenager menager = new PersonMenager();
+            var pets = new List<Pets>
+            {
+               new Pets { Type = "Cat", Name = "Ozi", Age = 5 },
+               new Pets { Type = "Dog", Name = "Cash", Age = 10}
+            };
+            var person = new Person
+            {
+                Name = "Mustafa",
+                Age = 24,
+                StateOfOrigin = "Turkey",
+                Pet = pets
+            };
+            menager.Add(person);
+            Console.ReadLine();
+        }
+    }
+}
